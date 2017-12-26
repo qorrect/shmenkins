0e69bd0b04b0fda8d91db5a1cd061534986b8aca
q <charlie.fats@gmail.com>
2017-12-26 13:20:46 -0600

    Moving to dynamically loading Jenkinsfile

--- README.md
+++ README.md
@@ -2,21 +2,22 @@
 
 A tiny library to run your Jenkinsfile through the commandline with 100% pass through ( it will execute everything locally not in any container ).
 
-To run simply, copy the shmenkins.groovy to your project directory, import it into your Jenkinsfile with
+To run , simply copy the shmenkins.groovy to your project directory, import it into your Jenkinsfile with
 
 ```java
 
-import static shmenkins.fileLoader
 import static shmenkins.*
+import static shmenkins.fileLoader
 
 ```
 
-and run the command with `groovy Jenkinsfile` , which will execute the stages.
+and run the command with `groovy Jenkinsfile` , which will execute the stages.  This stubs out any checkout logic, it assumes a Jenkinsfile in the project root.
 
 ## What is it
 
-Shmenkins tries to stub out most of the internal Jenkinsfile functions , if it doesnt please let me know and I'll add it.  Plugins like fileLoader above are additional static imports.
+Shmenkins tries to stub out most of the internal Jenkinsfile functions.  Plugins like fileLoader above are additional static imports.
 
 ### Plugins Supported
 
 [fileLoader](https://github.com/jenkinsci/workflow-remote-loader-plugin) - returns a text copy of the file from the directory its run in, so currently you have to link all your loaded files to the project root
+[configFileProvider](https://wiki.jenkins.io/display/JENKINS/Config+File+Provider+Plugin) - Currently does nothing, need a smarter way to default to sane values like ~/.m2/settings.xml
---
+++ x.groovy
@@ -0,0 +1,81 @@
+
+GroovyShell shell = new GroovyShell()
+def Jenkinsfile = shell.parse(new File("Jenkinsfile"))
+
+// Custom environment
+Jenkinsfile.MAVEN_SETTINGS = "~/.m2/settings.xml"
+Jenkinsfile.env = [ BRANCH : "dev" ]
+Jenkinsfile.params = [ DEPLOY_TARGET : "Do not deploy" ]
+// End Custom environment 
+
+def sh ( String s) {
+  print("Executing ${s}")
+  def p = ['/bin/bash','-c',s].execute()
+  p.waitFor()
+  def ret = p.text
+  println ret
+  return ret 
+}
+def sh (Map m) {
+  return sh(m['script'] )
+}
+
+Jenkinsfile.scm = ""
+Jenkinsfile.currentBuild = [ number : 1 , rawBuild: null ]
+Jenkinsfile.containerTemplate =  { Map m -> } 
+Jenkinsfile.hostPathVolume =  { o -> } 
+Jenkinsfile.podTemplate = { Map options, Closure body -> 
+  body()
+}
+Jenkinsfile.choice  = { Map o -> }
+Jenkinsfile.step  = { Map o -> }
+Jenkinsfile.parameters  = { List l -> }
+Jenkinsfile.git  = { Map o -> }
+Jenkinsfile.checkout =   { String s -> }
+Jenkinsfile.cron  = { String s -> }
+Jenkinsfile.pipelineTriggers  = { List l -> }
+Jenkinsfile.properties  = { List l -> }
+Jenkinsfile.disableConcurrentBuilds = {}
+Jenkinsfile.sh = this.&sh
+
+Jenkinsfile.readFile =  { String s ->
+  return new File(s).text
+}
+Jenkinsfile.node =  { String n = "",Closure body -> 
+  body()
+}
+Jenkinsfile.container=  { String s, Closure body -> 
+  println("Container ${s}")
+  body()
+}
+Jenkinsfile.stage=  {String s, Closure body -> 
+  println("Stage ${s}")
+  body()
+}
+
+Jenkinsfile.withCredentials= { List l, Closure body -> 
+  println("WithCredentials ${l}")
+  body()
+}
+Jenkinsfile.echo = { String s -> 
+  println(s)
+}
+
+//Plugins
+Jenkinsfile.configFile = { Map o }
+Jenkinsfile.configFileProvider  = { List l,Closure body -> 
+  body()
+}  
+
+class fileLoader {
+  static def fromGit(String libPath, String repository, String branch, String credentailsId, String labelExpression) {
+    GroovyShell shell = new GroovyShell()
+    def script = shell.evaluate(new File(libPath))
+    // def script = shell.parse(new File(libPath))
+    return script
+    
+  }
+}
+
+Jenkinsfile.fileLoader = fileLoader
+Jenkinsfile.run()
